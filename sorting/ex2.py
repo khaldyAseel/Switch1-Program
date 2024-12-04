f52a041269b075a53156c83035664ff637eb5b0c
khaldyAseel <132208072+khaldyAseel@users.noreply.github.com>
2024-12-04 17:02:32 +0000

    sorting

--- sorting/ex2.py
+++ sorting/ex2.py
@@ -1,25 +1,35 @@
 # 1 - merge sort with O(1) space 
 def merge_sort(arr):
     
-    if len(arr) > 1:
+    if len(arr)>1:
         mid = len(arr) // 2
         left = arr[:mid]
-        print(left)
         right = arr[mid:]
+
         merge_sort(left)
         merge_sort(right)
 
-    i = len(arr)-1
-    print(i)
-    while i>=0:
-        if left and right:
-            if left[i]>=right[i]:
-                arr[i] = left.pop()
+        i = 0
+        j = 0
+        k = 0
+        while i<len(left) and j<len(right):
+            if left[i]<=right[j]:
+                arr[k] = left[i]
+                i+=1
             else:
-                arr[i] = right.pop()
-        else:
-            arr[i] = left.pop() if left else right.pop()
-        i -=1
+                arr[k] = right[j]
+                j+=1
+            k+=1
+
+        while i<len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while j<len(right):
+            arr[k] = right[j]
+            j+=1
+            k+=1
 
     return arr    
     
@@ -35,18 +45,29 @@ def merge_sort_abs(arr):
         merge_sort_abs(left)
         merge_sort_abs(right)
 
-    i = len(arr)-1
-    while i>=0:
-        if len(left)>=1 and len(right)>=1:
-            if abs(left[i])>=abs(right[i]):
-                arr[i] = abs(left.pop())
+        i = 0
+        j = 0
+        k = 0
+        while i<len(left) and j<len(right):
+            if abs(left[i])<=abs(right[j]):
+                arr[k] = abs(left[i])
+                i+=1
             else:
-                arr[i] = abs(right.pop())
-        else:
-            arr[i] = abs(left.pop()) if len(left) ==1 else abs(right.pop())
-        i -=1
+                arr[k] = abs(right[j])
+                j+=1
+            k+=1
 
-        return arr  
+        while i<len(left):
+            arr[k] = abs(left[i])
+            i+=1
+            k+=1
+        
+        while j<len(right):
+            arr[k] = abs(right[j])
+            j+=1
+            k+=1
+
+    return arr
     
 
 # 3 - Sort list of strings – capital before lowercase
@@ -59,18 +80,29 @@ def merge_sort_strings(arr):
         merge_sort_strings(left)
         merge_sort_strings(right)
 
-    i = len(arr)-1
-    while i>=0:
-        if len(left)>=1 and len(right)>=1:
+        i = 0
+        j = 0
+        k = 0
+        while i<len(left) and j<len(right):
             if left[i].islower():
-                arr[i] = left.pop()
+                arr[k] = left[i]
+                i+=1
             else:
-                arr[i] = right.pop()
-        else:
-            arr[i] = left.pop() if len(left) ==1 else right.pop()
-        i -=1
+                arr[k] = right[j]
+                j+=1
+            k+=1
 
-        return arr
+        while i<len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while j<len(right):
+            arr[k] = right[j]
+            j+=1
+            k+=1
+
+    return arr
 
 # 4 - Sort numbers – odd should be before even
 def merge_sort_odd(arr):
@@ -82,18 +114,29 @@ def merge_sort_odd(arr):
         merge_sort_odd(left)
         merge_sort_odd(right)
 
-    i = len(arr)-1
-    while i>=0:
-        if len(left)>=1 and len(right)>=1:
-            if left[i] % 2 == 1:
-                arr[i] = left.pop()
+        i = 0
+        j = 0
+        k = 0
+        while i<len(left) and j<len(right):
+            if left[i]%2 == 1:
+                arr[k] = left[i]
+                i+=1
             else:
-                arr[i] = right.pop()
-        else:
-            arr[i] = left.pop() if len(left) ==1 else right.pop()
-        i -=1
+                arr[k] = right[j]
+                j+=1
+            k+=1
 
-        return arr  
+        while i<len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while j<len(right):
+            arr[k] = right[j]
+            j+=1
+            k+=1
+
+    return arr 
    
 # 5 - Sort numbers – primes before the rest
 def merge_sort_primes(arr):
@@ -105,18 +148,29 @@ def merge_sort_primes(arr):
         merge_sort_primes(left)
         merge_sort_primes(right)
 
-    i = len(arr)-1
-    while i>=0:
-        if len(left)>=1 and len(right)>=1:
-            if left[i].is_prime():
-                arr[i] = left.pop()
+        i = 0
+        j = 0
+        k = 0
+        while i<len(left) and j<len(right):
+            if is_prime(left[i]):
+                arr[k] = left[i]
+                i+=1
             else:
-                arr[i] = right.pop()
-        else:
-            arr[i] = left.pop() if len(left) ==1 else right.pop()
-        i -=1
+                arr[k] = right[j]
+                j+=1
+            k+=1
 
-        return arr 
+        while i<len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while j<len(right):
+            arr[k] = right[j]
+            j+=1
+            k+=1
+
+    return arr
 
 def is_prime(n):
     for i in range(2,n):
@@ -126,4 +180,8 @@ def is_prime(n):
 
 
 
-print(merge_sort([2,4,3,7,5]))
+print(f"merge sort with o(1) complexity:",{merge_sort([2,4,3,7,5])})
+print(f"merge sort with absolute values:",{merge_sort_abs([-1,-5,4,3,7])})
+print(f"merge sort with strings:",{merge_sort_strings(['a','A','b','C','d'])})
+print(f"merge sort first odd nums then even:",{merge_sort_odd([3,6,4,12,8])})
+print(f"merge sort with prime numbers:",{merge_sort_primes([2,6,8,5,3,7])})
--- sorting/ex3.py
+++ sorting/ex3.py
@@ -3,24 +3,49 @@ def quick_sort(arr,low=0,high=None):
     if high is None:
         high = len(arr) - 1
         
-    while low<high:
+    if low<high:
         pivot = high
         i = low -1
         for j in range(low,high):
-            if arr[i] > arr[pivot]:
-                temp = arr[pivot]
-                arr[pivot] = arr[i]
-                arr[i] = temp
+            if arr[j] < arr[pivot]:
                 i+=1
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
 
-        temp = arr[pivot]
-        arr[i + 1] = arr[pivot]
-        arr[pivot], arr[i + 1]
+        temp1 = arr[pivot]
+        arr[pivot] = arr[i+1]
+        arr[i+1] = temp1
         pivot = i + 1 
         quick_sort(arr, low, pivot - 1) 
         quick_sort(arr, pivot + 1, high)
     return arr 
 
 print(quick_sort([45,25,3,4,6]))
-print("aaa")
 
+# 2 - fizz buzz quick sort 
+def quick_sort_fizzbuzz(arr,low=0,high=None):   
+    if high is None:
+        high = len(arr) - 1
+        
+    if low<high:
+        pivot = high
+        i = low -1
+        for j in range(low,high):
+            if arr[j] < arr[pivot]:
+                i+=1
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+
+        temp1 = arr[pivot]
+        arr[pivot] = arr[i+1]
+        arr[i+1] = temp1
+        pivot = i + 1 
+        quick_sort(arr, low, pivot - 1) 
+        quick_sort(arr, pivot + 1, high)
+    return arr 
+
+
+
+print(quick_sort([45,25,3,4,6]))
