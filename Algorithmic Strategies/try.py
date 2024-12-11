52b3b7bff0192e328cc14b867f99ad6835baaa8c
khaldyAseel <132208072+khaldyAseel@users.noreply.github.com>
2024-12-11 15:22:22 +0000

    sorting

--- Algorithmic Strategies/try.py
+++ Algorithmic Strategies/try.py
@@ -36,3 +36,45 @@ def common_nums3(arr1,arr2,arr3):
 print(f"common nums between three arrays :{common_nums3([1,4,5,3],[2,3,6,7],[1,2,4,3])}")
 
 
+# 1.Count vowels
+def count_vowels(str):
+    vol = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for char in str:
+        if char in vol:
+            count+=1
+    return count 
+str = "computer"
+print(f"count of vowels in {str} is {count_vowels(str)}")
+print(f"count of constants in {str} is {len(str) - count_vowels(str)}")
+
+
+
+# 2.Median char 
+def median_char(str):
+
+    res = ''.join(sort_str(str))
+    if len(res)%2 == 0:
+        median = int((len(res)-1)/2)
+    else:
+        median = int((len(res))/2)
+    return res[median]
+
+# QuickSort forsorting the string 
+def sort_str(s):
+    chars = list(s)
+    if len(chars) <= 1:
+        return chars
+    else:
+        pivot = chars[0]
+        lesser = [c for c in chars[1:] if c <= pivot]
+        greater = [c for c in chars[1:] if c > pivot]
+        return sort_str(lesser) + [pivot] + sort_str(greater)
+
+print(f"median char is:{median_char('question')}")
+
+
+# 3.Word Score
+def word_score(str):
+    str = str.lower()
+    
