e2b0eca4cf56cef9c0c1470be20d23e67769387d
khaldyAseel <132208072+khaldyAseel@users.noreply.github.com>
2024-12-28 11:48:47 +0000

    ex1

--- question_solving/ex1.py
+++ question_solving/ex1.py
@@ -1,10 +1,18 @@
 # Write a simple camelCase method for strings. All words (except for the first) must have their first letter capitalized without spaces.
-
 # input : "camel is An animaL" -> "camelIsAnAnimal"
-# iterate over string 
 
-#def return_camel_case(str):
-#    returned_str = " "
+def return_camel_case(str):
+    returned_str = ""
+    for i in range(1,len(str)):
+        if str[i-1] == " ":
+            returned_str+= str[i].upper()
+        elif str[i]!= " ":
+            returned_str+=str[i].lower()
+    if str[0]!= " ":
+        returned_str = str[0]+returned_str
+    
+    return returned_str
 
-#    for i in range(0,len(str)-1):
-#        if str[i]!= " ":
+str = "camel is An animaL"
+print(f"camel case of input is: {return_camel_case(str)}")
+
