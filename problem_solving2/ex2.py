c6cd3fb79a4f903c51727e23ac5fbcedfd5c7c6d
khaldyAseel <132208072+khaldyAseel@users.noreply.github.com>
2025-01-01 07:42:06 +0000

    commit

--- problem_solving2/ex2.py
+++ problem_solving2/ex2.py
@@ -4,4 +4,35 @@
 # z = deepClone(y);  
 
 def deepClone(obj):
-    return 
+
+    if obj is None or isinstance(obj, (int, float, str, bool)):
+        return obj
+
+    # if there is an dictionaries
+    if isinstance(obj, dict):
+        cloned_dict = {}
+        for key, value in obj.items():
+            cloned_dict[deepClone(key)] = deepClone(value)
+        return cloned_dict
+
+    # if there is an lists
+    if isinstance(obj, list):
+        cloned_list = []
+        for item in obj:
+            cloned_list.append(deepClone(item))
+        return cloned_list
+
+    # if there is an tuples
+    if isinstance(obj, tuple):
+        cloned_tuple = tuple(deepClone(item) for item in obj)
+        return cloned_tuple
+
+
+# Input
+x = {"a": "b", "a2": ["first", "second"]}
+y = {"b": x, "b3": ["firstY", x]}
+z = deepClone(y)
+
+# Output
+print(z)
+
