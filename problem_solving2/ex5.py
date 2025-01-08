8aad411ca80e05f5c341022c7a993b9185f01d27
khaldyAseel <132208072+khaldyAseel@users.noreply.github.com>
2025-01-08 09:56:53 +0000

    commit

--- problem_solving2/ex5.py
+++ problem_solving2/ex5.py
@@ -5,18 +5,13 @@
 def missing_ch(word1,word2):
     charCounter = {}
     for ch in word1:
-        if charCounter.__contains__(ch):
-              charCounter[ch] +=1
-        charCounter[ch] = 1
+        charCounter[ch] = charCounter.get(ch,0)+ 1
     for ch in word2:
-        if charCounter.__contains__(ch):
-              charCounter[ch] +=1
-        charCounter[ch] = 1
-    print(charCounter)
+        charCounter[ch] = charCounter.get(ch,0)+ 1
     for letter in charCounter: 
-        if charCounter[letter] == 1:
+        if charCounter.get(letter) == 1:
             return True
-        return False
+    return False
         
 
 print(missing_ch('abc','ba'))
